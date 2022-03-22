e37d536b7edf3fa71ab2c6f28aeff275ca5e327f
Kyranz <kyranrafaeldd@gmail.com>
2022-03-22 15:15:45 -0300

    Estrutura de dados

--- teste.c
+++ teste.c
@@ -3,32 +3,37 @@
 #include <string.h>
 
 int main() {
-	int memory, i, j;
+	int memory, i, j, up_lim, low_lim;
     char *arr;
 	
     scanf("%i", &memory);
 
     if (memory >= 1024*1024) {
-        printf("Too mucch memory requested");
+        printf("Too much memory requested.");
         return 1;
     }
 
     arr = (char *) malloc(memory);
 
-    for (i = 0;i < memory;) {
-        for (j = 65; j < 91 && i < memory; j++, i++) {
+    for (i = 0; i < memory;) {
+        for (j = 65; j <= 90 && i < memory; j++, i++) {
             arr[i] = j;
         }
     }
 
-    for (i = 0; i < memory;) {
-        for (j = 0; j < 40 && i + j < memory; j++) {
-            printf("%c ", arr[i + j]);
+    low_lim = 0;
+    if (memory < 40) up_lim = memory;
+    else up_lim = 40;
+    do {
+        for (i = low_lim; i < up_lim; i++) {
+            printf("%c", arr[i]);
         }
-        printf("\nbr\n");
-        if (i + 40 > memory) i += memory - i;
-        else i += 40;
-    }
+        printf("\n");
+        low_lim += 40;
+        up_lim += 40;
+        if (up_lim > memory) up_lim = memory;
+        if (up_lim > 400) up_lim = 400;
+    } while (up_lim > low_lim);
 
 	return 0;
 }
